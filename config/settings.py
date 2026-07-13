78446ffca4a4b34f741d2f1db550fa61fc662123
pateltaksh2316 <pateltaksh558@gmail.com>
2026-07-13 10:42:36 +0530

    BLe and wifi done-2

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e^fh4g5q2(=7+_pw0g9+n)0vq$#h74z!%=hl@9x-c9gtx17r^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
